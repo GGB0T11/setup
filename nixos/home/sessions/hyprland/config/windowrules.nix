a96bcf2f1d3c21cb2295805cc89a254f4289d66b
GFreitasLab <gusttrabs@protonmail.com>
2025-12-09 07:55:56 -0300

    animação no rofi

--- nixos/home/sessions/hyprland/config/windowrules.nix
+++ nixos/home/sessions/hyprland/config/windowrules.nix
@@ -26,5 +26,8 @@
       "suppressevent maximize, class:.*"
       "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
     ];
+    layerrule = [
+      "animation slide top, rofi"
+    ];
   };
 }
