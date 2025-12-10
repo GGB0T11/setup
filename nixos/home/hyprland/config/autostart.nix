7793604135f0c037b161cbb4e45466f0386d5f87
GFreitasLab <gusttrabs@protonmail.com>
2025-12-10 13:26:52 -0300

    adicionei um exec-once para o swayosd funcionar sem problemas

--- nixos/home/hyprland/config/autostart.nix
+++ nixos/home/hyprland/config/autostart.nix
@@ -7,6 +7,7 @@
       "nm-applet"
       "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
       "gammastep -l -15.793889:-47.882778 -t 6500:3000"
+      "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=Hyprland"
     ];
   };
 }
